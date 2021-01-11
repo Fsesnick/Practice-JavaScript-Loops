352f4dd08b4ba898543756b6cb21b80bf28730b9
Fsesnick <fernandasesnick@gmail.com>
2021-01-11 17:19:14 +0000

    final version

--- js/app.js
+++ js/app.js
@@ -1,27 +1,24 @@
 const main = document.querySelector('main');
 const randomNumber = getRandomNumber(10);
-let tentativa;
-let numTentativas =0;
+
 let message ;
 
 function getRandomNumber(upper) {
   return Math.floor( Math.random() * upper ) + 1;
 }
 
-do{
-  
-tentativa = prompt('Estou pensando em um número entre 1 e 10. Qual é? ');
-numTentativas++;   
-  
+for (let i =0; i <= 10; i++){  
+ let tentativa = prompt('Estou pensando em um número entre 1 e 10. Qual é? ');
+ 
   if(parseInt(tentativa) === randomNumber){
-    message = ` Você precisou de ${numTentativas} tentativas para adivinhar o número ${randomNumber}`;
+    message = ` Você precisou de ${i} tentativas para adivinhar o número ${randomNumber}`;
     break;
   }else{
   message =` você não adivinhou o número. Era ${randomNumber}`;
   }
   
   
-}while (numTentativas <10); //10 tentativas apenas
+}
   
   
 //  3) Display a message letting the user know they guessed the number
